f14c082b47399fd7153754b414317960bd2fe651
Vignesh Ramesh <vigneshramesh448@gmail.com>
2025-06-19 11:27:13 +0530

    Create Multiply table.cpp

---
+++ Multiply table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+int i=1,n,mul;
+cin>>n;
+while((i<=n))
+{
+    mul=i*n;
+    cout<<i<<" "<<"X"<<" "<<n<<" "<<"="<<" "<<mul<<endl;
+    i++;
+}
+    return 0;
+}
